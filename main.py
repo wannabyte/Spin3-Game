c6ab3597fdffedae2ef08f595604d92f7c119c7d
xenia scullark <xscullark@gmail.com>
2023-08-16 19:53:32 -0400

    added bet

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+MAX_LINES = 3  # Define the maximum number of lines
 
 def deposit():
     while True:
@@ -12,3 +13,25 @@ def deposit():
             print("Please enter a number.")
     
     return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("How many lines would you like to bet (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Please select at least one line.")
+        else:
+            print("Please enter a number.")
+        
+    return lines
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+main()
+
